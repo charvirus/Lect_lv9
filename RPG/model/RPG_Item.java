b7617a955753c97b361a8120a08b539e196d0a58
Brandon J <43872889+charvirus@users.noreply.github.com>
2021-10-07 13:18:53 +0900

    # 13 unit 작성중

--- RPG/model/RPG_Item.java
+++ RPG/model/RPG_Item.java
@@ -1,5 +1,52 @@
 package model;
 
 public class RPG_Item {
+	static final int WEAPON = 1;
+	static final int ARMOR = 2;
+	static final int RING = 3;
+	private int kind;
+	private String name;
+	private int power;
+	private int price;
 
+	public void setItem(int k, String n, int p, int pr) {
+		this.kind = k;
+		this.name = n;
+		this.power = p;
+		this.price = pr;
+	}
+
+	public int getKind() {
+		return kind;
+	}
+
+	public void setKind(int kind) {
+		this.kind = kind;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
 }
--- RPG/model/RPG_Unit.java
+++ RPG/model/RPG_Unit.java
@@ -1,5 +1,71 @@
 package model;
 
 public class RPG_Unit {
+	String name;
+	int level;
+	int hp;
+	int maxHp;
+	int atk;
+	int def;
+	int exp;
+	boolean party;
+	RPG_Item weapon;
+	RPG_Item armor;
+	RPG_Item ring;
 
+	public RPG_Unit(String n, int l, int h, int a, int d, int e) {
+		this.name = n;
+		this.level = l;
+		this.maxHp = h;
+		this.atk = a;
+		this.def = d;
+		this.exp = e;
+		this.hp = this.maxHp;
+		this.party = false;
+		this.weapon = null;
+		this.armor = null;
+		this.ring = null;
+	}
+
+	public RPG_Unit(String n, int l, int h, int a, int d, int e, boolean p) {
+		this.name = n;
+		this.level = l;
+		this.maxHp = h;
+		this.atk = a;
+		this.def = d;
+		this.exp = e;
+		this.hp = this.maxHp;
+		this.party = p;
+		this.weapon = null;
+		this.armor = null;
+		this.ring = null;
+	}
+
+	public void setItem(RPG_Item w, RPG_Item a, RPG_Item r) {
+		this.weapon = w;
+		this.armor = a;
+		this.ring = r;
+	}
+
+	public void printStatus() {
+		System.out.print("[ 이름 : " + this.name + " ]");
+		System.out.print("[ 레벨 : " + this.level + " ]");
+		if (this.ring != null) {
+			System.out.print(" [ 체력 : " + hp + "+" + this.ring.getPower() + " ] ");
+		} else {
+			System.out.print(" [ 체력 : " + hp);
+		}
+		if(this.ring != null) {
+			System.out.println(" / "+this.maxHp+" + "+this.ring.getPower() +" ] ");
+		}else {
+			System.out.println(" / "+this.maxHp+" ] ");
+		}
+		if(this.weapon != null) {
+			System.out.print(" [ 공격력 : "+this.atk +" + "+this.weapon.getPower() + " ] ");
+		}else {
+			System.out.print(" [ 공격력 : "+this.atk );
+			
+		}
+		
+	}
 }
